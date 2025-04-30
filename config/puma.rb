dff7061b9efb73b1a2bf8e8099f9954c6ae5e167
moritti <morikko0124@yahoo.co.jp>
2025-05-01 05:54:09 +0900

    sslバインドをローカルのみにする

--- config/puma.rb
+++ config/puma.rb
@@ -1,18 +1,18 @@
 # config/puma.rb
 
-# ポート番号（Renderでは ENV["PORT"] が使われる）
-port ENV.fetch("PORT")
-
-# SSLバインド（ローカルで SSL を使う場合）
-if ENV["USE_LOCAL_SSL"] == "true"
-  ssl_bind '0.0.0.0', '3002', {
-    key:  "config/ssl/moriappli-emotion.com-key.pem",
-    cert: "config/ssl/moriappli-emotion.com.pem"
-  }
-end
+# Renderでは PORT 環境変数をそのまま使う（SSLバインドはしない）
+port ENV.fetch("PORT") { 3000 }
 
 threads_count = ENV.fetch("RAILS_MAX_THREADS", 3).to_i
 threads threads_count, threads_count
 
 plugin :tmp_restart
 pidfile ENV["PIDFILE"] if ENV["PIDFILE"]
+
+# ✅ ローカル開発時だけSSL使いたいならこう書く（Render上では false になるように）
+if ENV["USE_LOCAL_SSL"] == "true"
+  ssl_bind '0.0.0.0', '3002', {
+    key:  "config/ssl/moriappli-emotion.com-key.pem",
+    cert: "config/ssl/moriappli-emotion.com.pem"
+  }
+end
