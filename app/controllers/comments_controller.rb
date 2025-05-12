1df17b781ab4b8bc6c49bd33f5e86b9f29cee1e8
moritti <morikko0124@yahoo.co.jp>
2025-05-12 22:14:43 +0900

    コメント更新ボタン作成更新メッセージ反映

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,68 +1,89 @@
-# app/controllers/comments_controller.rb
 class CommentsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_emotion_log, only: [:create]
+  before_action :set_comment,      only: [:edit, :update, :destroy, :toggle_reaction]
 
   # POST /emotion_logs/:emotion_log_id/comments
-  # app/controllers/comments_controller.rb
-# app/controllers/comments_controller.rb
-def create
-  @comment = @emotion_log.comments.build(
-    body: params[:body],
-    user: current_user
-  )
-
-  respond_to do |format|
-    if @comment.save
-      format.turbo_stream   # create.turbo_stream.erb で新しいコメントを描画
-      format.html { redirect_to emotion_log_path(@emotion_log) }
-    else
-      format.html { redirect_to emotion_log_path(@emotion_log), alert: "コメントの投稿に失敗しました" }
-    end
-  end
-end
-
-
-  # GET /comments/:id/edit
-  def edit
-    respond_to do |format|
-      format.html             # app/views/comments/edit.html.erb
-      format.turbo_stream     # edit.turbo_stream.erb を置くなら
-    end
-  end
-
-  # PATCH /comments/:id
-  def update
-    return head(:forbidden) unless @comment.user == current_user
+  def create
+    @comment = @emotion_log.comments.build(
+      body: params[:body],
+      user: current_user
+    )
 
     respond_to do |format|
-      if @comment.update(comment_params)
-        format.turbo_stream   # update.turbo_stream.erb で差し替え
-        format.html { redirect_to emotion_log_path(@comment.emotion_log), notice: 'コメントを更新しました' }
+      if @comment.save
+        format.turbo_stream   # app/views/comments/create.turbo_stream.erb
+        format.html { redirect_to emotion_log_path(@emotion_log) }
       else
-        format.html { render :edit }
+        format.html { redirect_to emotion_log_path(@emotion_log), alert: 'コメントの投稿に失敗しました' }
       end
     end
   end
 
+  # GET /comments/:id/edit
+  def edit
+    respond_to do |format|
+      format.html            # app/views/comments/edit.html.erb
+      format.turbo_stream    # app/views/comments/edit.turbo_stream.erb (optional)
+    end
+  end
+
+  # PATCH /comments/:id
+  # app/controllers/comments_controller.rb (更新アクション)
+# PATCH /comments/:id
+# app/controllers/comments_controller.rb
+# app/controllers/comments_controller.rb
+def update
+  return head(:forbidden) unless @comment.user == current_user
+
+  if @comment.update(body: params.dig(:comment, :body) || params[:body])
+    render turbo_stream: [
+      turbo_stream.replace(
+        "comment_#{@comment.id}",
+        partial:  "comments/comment",
+        locals:   { comment: @comment }
+      ),
+      turbo_stream.append(
+        "flash",
+        "<div class=\"alert alert-success text-center\" role=\"alert\">コメントを更新しました</div>".html_safe
+      ),
+      turbo_stream.after(
+        "flash",
+        <<~JS
+          <script>
+            setTimeout(() => {
+              const f = document.getElementById("flash");
+              if (f) f.innerHTML = "";
+            }, 2000);
+          </script>
+        JS
+      )
+    ]
+  else
+    redirect_to emotion_log_path(@comment.emotion_log), alert: "コメントの更新に失敗しました"
+  end
+end
+
+
+
+
   # DELETE /comments/:id
   def destroy
-    # 自分のコメントだけを探す → 存在しなければ 404
-    @comment = current_user.comments.find_by(id: params[:id])
-    return head(:not_found) unless @comment
+    return head(:not_found)    unless @comment
+    return head(:forbidden)   unless @comment.user == current_user
 
-    @emotion_log = @comment.emotion_log
+    emotion_log = @comment.emotion_log
     @comment.destroy
 
     respond_to do |format|
-      format.turbo_stream   # destroy.turbo_stream.erb
-      format.html { redirect_to emotion_log_path(@emotion_log), notice: 'コメントを削除しました' }
+      format.turbo_stream    # app/views/comments/destroy.turbo_stream.erb
+      format.html { redirect_to emotion_log_path(emotion_log), notice: 'コメントを削除しました' }
     end
   end
 
   # POST /comments/:id/toggle_reaction
   def toggle_reaction
-    kind = params[:kind].to_sym
+    kind     = params[:kind].to_sym
     reaction = @comment.comment_reactions.find_by(user: current_user, kind: kind)
 
     if reaction
@@ -77,12 +98,10 @@ end
   private
 
   def set_emotion_log
-    @emotion_log = EmotionLog.find(params[:emotion_log_id])
+    @emotion_log = EmotionLog.find(params[:emotion_log_id]) if params[:emotion_log_id]
   end
 
   def set_comment
-    # 他アクション（edit/update/toggle_reaction）用に一般ロード
     @comment = Comment.includes(:user, :comment_reactions).find_by(id: params[:id])
   end
-
 end
