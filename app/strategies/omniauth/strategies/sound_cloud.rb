6a085862a77c0360c7c0b4ff711abe60b629af3e
moritti <morikko0124@yahoo.co.jp>
2025-05-22 02:40:43 +0900

    naosi

--- app/strategies/omniauth/strategies/sound_cloud.rb
+++ app/strategies/omniauth/strategies/sound_cloud.rb
@@ -1,34 +1,38 @@
 require 'omniauth-oauth2'
 
-class OmniAuth::Strategies::SoundCloud < OmniAuth::Strategies::OAuth2
-  option :name, 'soundcloud'
+module OmniAuth
+  module Strategies
+    class SoundCloud < OmniAuth::Strategies::OAuth2
+      option :name, 'soundcloud'
 
-  option :client_options, {
-    site: 'https://api.soundcloud.com',
-    authorize_url: 'https://secure.soundcloud.com/authorize',
-    token_url: 'https://secure.soundcloud.com/oauth/token'
-  }
+      option :client_options, {
+        site: 'https://api.soundcloud.com',
+        authorize_url: 'https://secure.soundcloud.com/authorize',
+        token_url: 'https://secure.soundcloud.com/oauth/token'
+      }
 
-  option :authorize_params, {
-    client_id: ENV['SOUNDCLOUD_CLIENT_ID']
-  }
+      option :authorize_params, {
+        client_id: ENV['SOUNDCLOUD_CLIENT_ID']
+      }
 
-  option :token_params, {
-    client_id: ENV['SOUNDCLOUD_CLIENT_ID'],
-    client_secret: ENV['SOUNDCLOUD_CLIENT_SECRET'],
-    redirect_uri: ENV['SOUNDCLOUD_REDIRECT_URI']
-  }
+      option :token_params, {
+        client_id: ENV['SOUNDCLOUD_CLIENT_ID'],
+        client_secret: ENV['SOUNDCLOUD_CLIENT_SECRET'],
+        redirect_uri: ENV['SOUNDCLOUD_REDIRECT_URI']
+      }
 
-  uid { raw_info['id'] }
+      uid { raw_info['id'] }
 
-  info do
-    {
-      name: raw_info['username'],
-      image: raw_info['avatar_url']
-    }
-  end
+      info do
+        {
+          name: raw_info['username'],
+          image: raw_info['avatar_url']
+        }
+      end
 
-  def raw_info
-    @raw_info ||= access_token.get('/me').parsed
+      def raw_info
+        @raw_info ||= access_token.get('/me').parsed
+      end
+    end
   end
 end
