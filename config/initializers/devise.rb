7feba19e89b0350b4329bea4f80cd24dc9795bdb
moritti <morikko0124@yahoo.co.jp>
2025-05-22 04:35:33 +0900

    renderとsoundcloud競合設定解決

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,4 +1,41 @@
-require_relative '../../lib/omniauth/strategies/sound_cloud'
+require 'omniauth-oauth2'
+
+module OmniAuth
+  module Strategies
+    class SoundCloud < OmniAuth::Strategies::OAuth2
+      option :name, 'soundcloud'
+
+      option :client_options, {
+        site: 'https://api.soundcloud.com',
+        authorize_url: 'https://secure.soundcloud.com/authorize',
+        token_url: 'https://secure.soundcloud.com/oauth/token'
+      }
+
+      option :authorize_params, {
+        client_id: ENV['SOUNDCLOUD_CLIENT_ID']
+      }
+
+      option :token_params, {
+        client_id: ENV['SOUNDCLOUD_CLIENT_ID'],
+        client_secret: ENV['SOUNDCLOUD_CLIENT_SECRET'],
+        redirect_uri: ENV['SOUNDCLOUD_REDIRECT_URI']
+      }
+
+      uid { raw_info['id'] }
+
+      info do
+        {
+          name: raw_info['username'],
+          image: raw_info['avatar_url']
+        }
+      end
+
+      def raw_info
+        @raw_info ||= access_token.get('/me').parsed
+      end
+    end
+  end
+end
 
 Devise.setup do |config|
   ENV['SOUNDCLOUD_REDIRECT_URI'] = 'https://moriappli-emotion.com/auth/soundcloud/callback'
