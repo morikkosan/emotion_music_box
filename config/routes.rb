5370c300fd6e9f8138d2ddfea3396e1110396fc6
moritti <morikko0124@yahoo.co.jp>
2025-06-04 00:19:35 +0900

    プレイリストモーダルが出るまで

--- config/routes.rb
+++ config/routes.rb
@@ -32,7 +32,7 @@ Rails.application.routes.draw do
   end
 
 
-  resources :playlists, only: [:create, :index, :show, :edit, :update]
+  resources :playlists, only: [:new, :create, :index, :show, :edit, :update]
 
   get :my_emotion_logs,          to: 'emotion_logs#my_emotion_logs'
   get 'jamendo/search',          to: 'jamendo#search'
