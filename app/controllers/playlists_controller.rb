ddd5debf9dd5e1f9997232ffdc646624cddeebf5
moritti <morikko0124@yahoo.co.jp>
2025-06-04 18:34:37 +0900

    playlist_toastclear

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -17,18 +17,20 @@ def new
 end
 
  def create
-  @playlist = current_user.playlists.create!(playlist_params)
-  (params[:selected_logs] || []).each do |log_id|
-    @playlist.playlist_items.create!(emotion_log_id: log_id)
+    @playlist = current_user.playlists.create!(playlist_params)
+    (params[:selected_logs] || []).each do |log_id|
+      @playlist.playlist_items.create!(emotion_log_id: log_id)
+    end
+    respond_to do |format|
+      format.turbo_stream {
+        render turbo_stream: [
+          turbo_stream.remove("modal-container"),
+          turbo_stream.append("comment-container", partial: "shared/show_toast", locals: { message: "プレイリストを作成しました！" })
+        ]
+      }
+      format.html { redirect_to playlists_path, notice: "プレイリストを作成しました！" }
+    end
   end
-  respond_to do |format|
-    format.turbo_stream {
-      # モーダルだけ閉じるだけ！JSはコントローラ側で発火
-      render turbo_stream: turbo_stream.remove("modal-container")
-    }
-    format.html { redirect_to playlists_path, notice: "プレイリストを作成しました！" }
-  end
-end
 
 
 
