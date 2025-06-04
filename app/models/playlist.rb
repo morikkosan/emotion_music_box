ec576e5f1c9fbbd00ebdafd63b51dc0dbcd0209a
moritti <morikko0124@yahoo.co.jp>
2025-06-05 01:44:16 +0900

    playlist_createmade

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -2,4 +2,8 @@ class Playlist < ApplicationRecord
   belongs_to :user
   has_many :playlist_items, dependent: :destroy
   has_many :emotion_logs, through: :playlist_items
+
+  validates :name,
+            presence: { message: "を入力してください" },
+            length:   { maximum: 15, message: "は15文字以内で入力してください" }
 end
