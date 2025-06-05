23e1c44523308e31bdc63fac7018e4236b48829b
moritti <morikko0124@yahoo.co.jp>
2025-06-06 02:28:51 +0900

    プレイリスト削除フラッシュメッセージ処理

--- config/routes.rb
+++ config/routes.rb
@@ -32,7 +32,9 @@ Rails.application.routes.draw do
   end
 
 
-  resources :playlists, only: [:new, :create, :index, :show, :edit, :update]
+  resources :playlists, only: [:new, :create, :index, :show, :edit, :update, :destroy] do
+      resources :playlist_items, only: [:destroy]
+  end
 
   get :my_emotion_logs,          to: 'emotion_logs#my_emotion_logs'
   get 'jamendo/search',          to: 'jamendo#search'
