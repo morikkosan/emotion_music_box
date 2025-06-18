689170594289c79c65ba932f9fc03a90f30f2f16
moritti <morikko0124@yahoo.co.jp>
2025-06-18 18:36:07 +0900

    seeds作成　#112

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,27 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+user = User.find_or_create_by!(email: 'test@example.com') do |u|
+  u.password = 'password'
+  u.name = 'Test User'
+  u.uid = 'test_User'
+  u.provider = "email"
+  u.profile_completed = true
+end
+
+tag = Tag.find_or_create_by!(name: "happy")
+
+emotion_log = EmotionLog.find_or_create_by!(user: user, emotion: "happy", date: Date.today) do |log|
+  log.description = "CI用のテスト投稿"
+  log.track_name = "Test Song"
+  log.music_url = "https://example.com/art.jpg"
+end
+
+EmotionLogTag.find_or_create_by!(emotion_log: emotion_log, tag: tag)
+
+comment = Comment.find_or_create_by!(user: user, emotion_log: emotion_log, body: "テキストコメント")
+
+CommentReaction.find_or_create_by!(user: user, comment: comment, kind: 1)
+
+playlist = Playlist.find_or_create_by!(user: user, name: "Test Playlist")
+
+PlaylistItem.find_or_create_by!(playlist: playlist, emotion_log: emotion_log)
+
+Bookmark.find_or_create_by!(user: user, emotion_log: emotion_log)
