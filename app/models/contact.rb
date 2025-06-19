6b886b77a83045902ae3c6e558a7c956668031ce
moritti <morikko0124@yahoo.co.jp>
2025-06-19 21:19:12 +0900

    feat:お問い合わせドームの作成とバリデーション追加　#fixes #113

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -3,7 +3,11 @@ class Contact
 
   attr_accessor :name, :email, :message
 
-  validates :name, presence: true
-  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP}
-  validates :message, presence: true
+  validates :name,    presence: true, length: { minimum: 2, maximum: 10 }
+  validates :email,   presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :message, presence: true, length: { minimum: 5, maximum: 200 }
+
+  def persisted?
+    false
+  end
 end
