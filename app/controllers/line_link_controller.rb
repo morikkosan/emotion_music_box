58edf67b1b541b4b7ca66146de5279355b1fb107
moritti <morikko0124@yahoo.co.jp>
2025-06-25 17:34:39 +0900

    test:syntaxerror直し　#117

--- app/controllers/line_link_controller.rb
+++ app/controllers/line_link_controller.rb
@@ -2,17 +2,18 @@ class LineLinkController < ApplicationController
   before_action :authenticate_user!
 
   def link
-  token = params[:token]
-  line_user_id = params[:line_user_id]
+    token = params[:token]
+    line_user_id = params[:line_user_id]
 
-  link_token = LineLinkToken.find_by(token: token, used: false)
+    link_token = LineLinkToken.find_by(token: token, used: false)
 
-  if link_token.present? && line_user_id.present?
-    link_token.user.update!(line_user_id: line_user_id)
-    link_token.update!(used: true)
-    render :success
-  else
-    render plain: "⚠️ 無効なリンクまたは連携済みです。", status: :unprocessable_entity
+    if link_token.present? && line_user_id.present?
+      link_token.user.update!(line_user_id: line_user_id)
+      link_token.update!(used: true)
+      render :success
+    else
+      render plain: "⚠️ 無効なリンクまたは連携済みです。", status: :unprocessable_entity
+    end
   end
 end
 
