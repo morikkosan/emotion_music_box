db2a5af919a59856f82f4c10408df82bf6a572ed
moritti <morikko0124@yahoo.co.jp>
2025-07-03 21:20:17 +0900

    refactor:csp対策css #124

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -1,27 +1,25 @@
-# # Be sure to restart your server when you modify this file.
+# Be sure to restart your server when you modify this file.
 
+Rails.application.configure do
+  config.content_security_policy do |policy|
+    policy.default_src :self, :https
+    policy.font_src    :self, :https, "https://fonts.gstatic.com"
+    policy.style_src   :self, :https, "https://fonts.googleapis.com"
+    policy.img_src     :self, :https, "https://res.cloudinary.com", :data
+    policy.object_src  :none
+    policy.script_src  :self, :https
+    # 必要に応じて追加
+  end
+
+  config.content_security_policy_nonce_generator = ->(request) { request.session.id.to_s }
+  config.content_security_policy_nonce_directives = %w(script-src style-src)
+end
+
+
+# === 以下参考用 ===
 # # Define an application-wide content security policy.
 # # See the Securing Rails Applications Guide for more information:
 # # https://guides.rubyonrails.org/security.html#content-security-policy-header
-
-# Rails.application.configure do
-#   config.content_security_policy do |policy|
-#     policy.default_src :self, :https
-#     policy.font_src    :self, :https, :data
-#     policy.img_src     :self, :https, :data
-#     policy.object_src  :none
-#     policy.script_src  :self, :https
-#     policy.style_src   :self, :https
-#     # Specify URI for violation reports
-#     # policy.report_uri "/csp-violation-report-endpoint"
-#   end
-
-#   # Generate session nonces for permitted importmap, inline scripts, and inline styles.
-#   config.content_security_policy_nonce_generator = ->(request) { request.session.id.to_s }
-#   config.content_security_policy_nonce_directives = %w(script-src style-src)
-
-#   # Report violations without enforcing the policy.
-#   # config.content_security_policy_report_only = true
-# end
-
-# ＊＊！！！！注意ここはそのうちやるべきところですがコメントアウトしたらスタイルが崩れますちcspの調整しながらやるべき
+#
+# config.content_security_policy_report_only = true
+# policy.report_uri "/csp-violation-report-endpoint"
