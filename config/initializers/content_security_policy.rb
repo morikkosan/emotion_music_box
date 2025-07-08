6e9b7e5c12a89aa3601d46e516acf6e4464c5a66
moritti <morikko0124@yahoo.co.jp>
2025-07-08 18:33:39 +0900

    feat:style修正

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -1,17 +1,17 @@
-Rails.application.configure do
-  config.content_security_policy do |policy|
-    policy.default_src :self, :https
-    policy.font_src    :self, :https, "https://fonts.gstatic.com"
-    policy.style_src   :self, :https, "https://fonts.googleapis.com"
-    policy.img_src     :self, :https, "https://res.cloudinary.com", :data
-    policy.object_src  :none
-    policy.script_src  :self, :https
-    # 必要に応じて report_uri も指定可能
-    # policy.report_uri "/csp-violation-report-endpoint"
-  end
+# Rails.application.configure do
+#   config.content_security_policy do |policy|
+#     policy.default_src :self, :https
+#     policy.font_src    :self, :https, "https://fonts.gstatic.com"
+#     policy.style_src   :self, :https, "https://fonts.googleapis.com"
+#     policy.img_src     :self, :https, "https://res.cloudinary.com", :data
+#     policy.object_src  :none
+#     policy.script_src  :self, :https
+#     # 必要に応じて report_uri も指定可能
+#     # policy.report_uri "/csp-violation-report-endpoint"
+#   end
 
-  config.content_security_policy_nonce_generator = ->(request) { request.session.id.to_s }
-  config.content_security_policy_nonce_directives = %w(script-src style-src)
+#   config.content_security_policy_nonce_generator = ->(request) { request.session.id.to_s }
+#   config.content_security_policy_nonce_directives = %w(script-src style-src)
 
-  config.content_security_policy_report_only = true  # ここが報告モードの設定
-end
+#   config.content_security_policy_report_only = true  # ここが報告モードの設定
+# end
