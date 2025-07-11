2ec2a20a135d748c1606b3352e51fb910b1f7de0
moritti <morikko0124@yahoo.co.jp>
2025-07-12 00:13:03 +0900

    feat:rspec初期設定完了

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,14 +1,12 @@
 FactoryBot.define do
   factory :user do
-    sequence(:email) { |n| "user#{n}@example.com" 0 }
+    sequence(:email) { |n| "user#{n}@example.com" }
     password { "password" }
     name { "Test User" }
     uid { SecureRandom.hex(8) }
     provider { "sound_cloud" }
     profile_completed { true }
-    gender { "other"}
+    gender { "other" }
     age { 20 }
   end
 end
-
-
