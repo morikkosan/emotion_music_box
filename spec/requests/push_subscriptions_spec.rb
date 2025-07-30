68caaed58155622340e49e3240019f9288cce459
moritti <morikko0124@yahoo.co.jp>
2025-07-31 01:27:35 +0900

    test: tag_controllerとその他のテストに使わないコントローラー除外　#128

--- spec/requests/push_subscriptions_spec.rb
+++ spec/requests/push_subscriptions_spec.rb
@@ -28,6 +28,25 @@ RSpec.describe "PushSubscriptions", type: :request do
         expect(user.reload.push_subscription).to be_present
       end
     end
+    
+    context "異常系（バリデーションエラーなど）" do
+  before do
+    sign_in user
+    # create_push_subscription! を失敗させるためバリデーション違反データを送る
+    allow_any_instance_of(User).to receive(:create_push_subscription!).and_raise(StandardError, "強制エラー")
+  end
+
+  it "失敗レスポンス（422）が返る" do
+    post push_subscription_path,
+      params: { subscription: valid_subscription }.to_json,
+      headers: headers
+
+    expect(response).to have_http_status(:unprocessable_entity)
+    expect(JSON.parse(response.body)["success"]).to eq(false)
+    expect(JSON.parse(response.body)["error"]).to eq("強制エラー")
+  end
+end
+
 
     context "ログインしていない場合" do
       it "401 Unauthorized または 403 Forbidden を返す" do
