96ecaeb9a280eadc11de8c1d6ccf531f610df540
moritti <morikko0124@yahoo.co.jp>
2025-08-15 19:10:40 +0900

    test:application.jsの内容テスト#231 #235

--- test/setup/jest.setup.js
+++ test/setup/jest.setup.js
@@ -1,19 +1,67 @@
-// 各テスト実行前の共通セットアップ
+// test/setup/jest.setup.js
+import 'whatwg-fetch';
 
-// console の不要な出力を抑える（必要に応じてコメントアウト）
-beforeEach(() => {
-  jest.spyOn(console, 'warn').mockImplementation(() => {});
-  jest.spyOn(console, 'error').mockImplementation(() => {});
-});
+// ---- jest-dom の matcher を“安全に” extend（default 等の非関数を除外）----
+import * as rawMatchers from '@testing-library/jest-dom/matchers';
+const matchers = Object.fromEntries(
+  Object.entries(rawMatchers).filter(([, v]) => typeof v === 'function')
+);
+expect.extend(matchers);
 
-afterEach(() => {
-  jest.restoreAllMocks();
-});
-
-// SweetAlert2 の最低限モック
-if (!global.window) global.window = {};
-if (!window.Swal) {
-  window.Swal = {
-    fire: jest.fn().mockResolvedValue({ isConfirmed: true }),
-  };
+// ---- PointerEvent の安全ポリフィル（既にあれば何もしない）----
+if (!global.PointerEvent) {
+  class PEvent extends MouseEvent {
+    constructor(type, opts = {}) {
+      // button/buttons は MouseEvent の init で渡す（後から代入しない）
+      super(type, opts);
+      Object.defineProperty(this, 'pointerId',  { value: opts.pointerId ?? 1, enumerable: true });
+      Object.defineProperty(this, 'isPrimary',  { value: opts.isPrimary ?? true, enumerable: true });
+      if (opts.pointerType) {
+        Object.defineProperty(this, 'pointerType', { value: opts.pointerType, enumerable: true });
+      }
+    }
+  }
+  global.PointerEvent = PEvent;
 }
+
+// jsdom に無い API をダミーで生やす（存在すれば何もしない）
+if (!Element.prototype.setPointerCapture) {
+  Object.defineProperty(Element.prototype, 'setPointerCapture', {
+    value: function () {},
+    configurable: true,
+    writable: true,
+  });
+}
+if (!Element.prototype.releasePointerCapture) {
+  Object.defineProperty(Element.prototype, 'releasePointerCapture', {
+    value: function () {},
+    configurable: true,
+    writable: true,
+  });
+}
+
+// ---- テスト全体で noisy なログを抑制（非同期ログも拾えるよう beforeAll/afterAll）----
+let origWarn, origError;
+
+beforeAll(() => {
+  origWarn  = console.warn.bind(console);
+  origError = console.error.bind(console);
+
+  // ここで丸ごと封じる：テストが“意図的に”error を期待していない限り安全
+  // （もし特定テストで console.error を検証したい場合は、そのテスト内で
+  //  いったん jest.spyOn(console, 'error') し直せばOK）
+  // console.log はそのまま残す
+  // eslint-disable-next-line no-console
+  console.warn  = jest.fn(() => {});
+  // eslint-disable-next-line no-console
+  console.error = jest.fn(() => {});
+});
+
+afterAll(() => {
+  // 元に戻す（watch モードでも積み上がらないように）
+  if (origWarn)  console.warn  = origWarn;
+  if (origError) console.error = origError;
+});
+
+// ※ DOM の消去や MutationObserver の差し替え等はここではしない。
+//   （それらは spec/javascripts/setupTests.js に任せる）
