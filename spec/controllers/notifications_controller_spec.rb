0d9ffdd7d4d44828532e24366a657833d9d029e2
moritti <morikko0124@yahoo.co.jp>
2025-08-19 18:31:43 +0900

    chore:cifileににrspecとjestを読みこむよう追加　#236

--- spec/controllers/notifications_controller_spec.rb
+++ spec/controllers/notifications_controller_spec.rb
@@ -4,9 +4,15 @@ require 'web-push'
 RSpec.describe NotificationsController, type: :controller do
   let(:user) { create(:user) }
 
+  # Deviseのマッピング＋ログインを各ケースで使えるように
+  before do
+    @request.env['devise.mapping'] = Devise.mappings[:user]
+  end
+
   describe "GET #test" do
     context "プッシュ購読情報がある場合" do
       before do
+        sign_in user
         create(:push_subscription, user: user)
         allow(WebPush).to receive(:payload_send).and_return(true)
       end
@@ -20,6 +26,8 @@ RSpec.describe NotificationsController, type: :controller do
     end
 
     context "プッシュ購読情報がない場合" do
+      before { sign_in user }
+
       it "通知を送信せず、メッセージを返す" do
         get :test, params: { id: user.id }
         expect(response).to have_http_status(:success)
@@ -29,7 +37,10 @@ RSpec.describe NotificationsController, type: :controller do
   end
 
   describe "GET #public_key" do
-    before { ENV['VAPID_PUBLIC_KEY'] = 'test_public_key' }
+    before do
+      ENV['VAPID_PUBLIC_KEY'] = 'test_public_key'
+      sign_in user
+    end
 
     it "VAPID公開鍵を返す" do
       get :public_key
