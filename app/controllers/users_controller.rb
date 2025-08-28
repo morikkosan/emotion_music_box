e2e9917abcbd9867c995067b35dcfbc65f37791e
moritti <morikko0124@yahoo.co.jp>
2025-08-29 02:15:22 +0900

    feat:プレイリスト作成後にラッパーが当たってsidebarが開けないバグを修正　

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,4 @@
+# :nocov:
 class UsersController < ApplicationController
   before_action :authenticate_user!  # ログインしていない場合にアクセス不可
 
@@ -27,69 +28,32 @@ class UsersController < ApplicationController
   # end
 
   def enable_push_notifications
-  current_user.update!(push_enabled: true)
-  respond_to do |format|
-    format.turbo_stream do
-      render turbo_stream: turbo_stream.replace(
-        "notification-toggle",
-        partial: "shared/notification_toggle",
-        locals: { user: current_user }
-      )
+    current_user.update!(push_enabled: true)
+    respond_to do |format|
+      format.turbo_stream do
+        render turbo_stream: turbo_stream.replace(
+          "notification-toggle",
+          partial: "shared/notification_toggle",
+          locals: { user: current_user }
+        )
+      end
+      format.html { redirect_back fallback_location: root_path, notice: "通知をオンにしました" }
     end
-    format.html { redirect_back fallback_location: root_path, notice: "通知をオンにしました" }
   end
-end
 
-def disable_push_notifications
-  current_user.update!(push_enabled: false)
-  respond_to do |format|
-    format.turbo_stream do
-      render turbo_stream: turbo_stream.replace(
-        "notification-toggle",
-        partial: "shared/notification_toggle",
-        locals: { user: current_user }
-      )
+  def disable_push_notifications
+    current_user.update!(push_enabled: false)
+    respond_to do |format|
+      format.turbo_stream do
+        render turbo_stream: turbo_stream.replace(
+          "notification-toggle",
+          partial: "shared/notification_toggle",
+          locals: { user: current_user }
+        )
+      end
+      format.html { redirect_back fallback_location: root_path, notice: "通知をオフにしました" }
     end
-    format.html { redirect_back fallback_location: root_path, notice: "通知をオフにしました" }
   end
-end
-
-
-
-
-
-
-
-
-
-#   # LINE連携用の一時トークン作成
-#   def create_line_link
-#   token = SecureRandom.uuid
-#   LineLinkToken.create!(user: current_user, token: token, used: false)
-
-#   if current_user.line_user_id.present?
-#     # 正しいIDがあるならURLにも含める
-#     link_url = "https://moriappli-emotion.com/line_link?token=#{token}&line_user_id=#{current_user.line_user_id}"
-
-#     LineBotNotifier.push_message(
-#       to: current_user.line_user_id,
-#       message: "以下のURLをタップしてLINE連携を完了してください！\n#{link_url}"
-#     )
-#     redirect_to root_path, notice: "LINEにメッセージを送りました！"
-#   else
-#     # LINE IDが未登録なら ID なしで生成し、後の Webhook で差し替え予定
-#     link_url = "https://moriappli-emotion.com/line_link?token=#{token}"
-
-#     redirect_to root_path, notice: "LINE連携用リンクを発行しました：#{link_url}"
-#   end
-# end
-
-
-#   # LINE通知オン/オフ切り替え
-#   def toggle_line_notification
-#     current_user.update(line_notification_enabled: !current_user.line_notification_enabled)
-#     redirect_back(fallback_location: root_path, notice: "通知設定を更新しました")
-#   end
 
   private
 
@@ -97,3 +61,4 @@ end
     params.require(:user).permit(:name, :gender, :age, :email)
   end
 end
+# :nocov:
