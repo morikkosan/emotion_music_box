8d3629c4449a94c6e51af846adf33626ddb546d5
moritti <morikko0124@yahoo.co.jp>
2025-08-31 02:15:01 +0900

    test: 日付のズレ修正　#98

--- spec/factories/emotion_logs.rb
+++ spec/factories/emotion_logs.rb
@@ -1,10 +1,9 @@
-# spec/factories/emotion_logs.rb
 FactoryBot.define do
   factory :emotion_log do
     association :user
     emotion { "気分良い" }
     description { "テスト投稿" }
-    date { Date.today }
+    date { Time.zone.today }  # ← ここを Date.today から Time.zone.today に変更
     music_url { "https://soundcloud.com/example-track" }
     track_name { "Test Song" }
     music_art_url { "https://i1.sndcdn.com/artworks-abc123-t500x500.jpg" }
