2dad0e3cda25085044f53443b23fdd7cb4f52fbb
moritti <morikko0124@yahoo.co.jp>
2025-09-01 23:51:10 +0900

    fix:メール設定見直し　#98

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,8 @@
+# app/mailers/application_mailer.rb
 class ApplicationMailer < ActionMailer::Base
-  default from: "from@example.com"
+  # ★ここを必ずあなたのドメインの認証済みアドレスに
+  # 例: no-reply@moriappli-emotion.com（SendGrid/Mailgun/SES 等で認証）
+  default from: "Emotion Music Box <no-reply@moriappli-emotion.com>"
   layout "mailer"
 end
+ 
