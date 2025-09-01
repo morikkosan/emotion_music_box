866d32b3b0f44c208eb7479b0593bd9f471f6593
moritti <morikko0124@yahoo.co.jp>
2025-09-02 01:10:06 +0900

    fix:メールをyahooに変える　#98

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,23 +1,19 @@
 # app/mailers/contact_mailer.rb
 class ContactMailer < ApplicationMailer
-  # controller 側で: ContactMailer.with(contact: params[:contact]).notify_admin.deliver_now を想定
   def notify_admin
-    @contact = params[:contact] # Contactオブジェクト or {name:, email:, message:} のどちらも想定
+    @contact = params[:contact]  # Contact or {name:, email:, message:}
 
-    # 両対応：オブジェクトなら .name、ハッシュなら [:name]
     name    = @contact.respond_to?(:name)    ? @contact.name    : @contact[:name]
     email   = @contact.respond_to?(:email)   ? @contact.email   : @contact[:email]
     message = @contact.respond_to?(:message) ? @contact.message : @contact[:message]
-
-    # ビューでも使えるように
-    @name    = name
+    @name = name
     @message = message
 
     mail(
       to: "morikko0124@gmail.com",
       subject: "【お問い合わせ】新規メッセージが届きました",
-      from: "Emotion Music Box <no-reply@moriappli-emotion.com>",
-      reply_to: email.presence || "no-reply@moriappli-emotion.com",
+      # From は ApplicationMailer（Yahoo）を継承
+      reply_to: email.presence || "morikko0124@yahoo.co.jp",
       message_id: "<contact-#{SecureRandom.hex(16)}@moriappli-emotion.com>"
     )
   end
