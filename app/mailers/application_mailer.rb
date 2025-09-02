a4c4287d8b73851ed71b117e6debe7e7d03c25ef
moritti <morikko0124@yahoo.co.jp>
2025-09-02 18:22:28 +0900

    fix:メールをresendに変更＆モバイルshowのxリンク消してた　#98

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,7 @@
+# app/mailers/application_mailer.rb
 class ApplicationMailer < ActionMailer::Base
-  default from: "yuki mori <morikko0124@yahoo.co.jp>"
+  # ドメイン認証が済むまでの暫定 → "onboarding@resend.dev"
+  # 認証後は no-reply@moriappli-emotion.com に切替
+  default from: "Emotion Music Box <no-reply@moriappli-emotion.com>"
   layout "mailer"
 end
