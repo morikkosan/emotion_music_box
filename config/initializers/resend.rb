a02ad9becac26349d1e8c47f31428ab8f62b3dfa
moritti <morikko0124@yahoo.co.jp>
2025-09-03 04:22:54 +0900

    fix:メール本番コンソールテスト完了　#98

--- config/initializers/resend.rb
+++ config/initializers/resend.rb
@@ -1,9 +1,14 @@
+# /config/initializers/resend.rb
+
 # このファイルを明示 require（安全のため）
 require Rails.root.join("app/mailers/delivery_methods/resend_delivery_method").to_s
 
+require "resend"
+
+# Render の Environment から読み込み
 Resend.api_key = ENV["RESEND_API_KEY"]
 
-# ここは DeliveryMethods を使う
+# ActionMailer に独自 delivery method を登録
 ActionMailer::Base.add_delivery_method :resend, ::DeliveryMethods::ResendDeliveryMethod
 
 Rails.logger.info "[Resend] API key present? #{Resend.api_key.present?}"
