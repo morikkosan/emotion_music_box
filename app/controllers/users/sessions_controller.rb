02c6f696cd2436f995d91aef4ce49fa7b39c4da7
moritti <morikko0124@yahoo.co.jp>
2025-09-18 22:48:04 +0900

    refactor: コントローラーdebugリファクター

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,5 +1,5 @@
 class Users::SessionsController < Devise::SessionsController
-   def destroy
+  def destroy
     if current_user
       current_user.update(
         soundcloud_token: nil,
@@ -8,25 +8,17 @@ class Users::SessionsController < Devise::SessionsController
       )
       Rails.logger.info "🟢 SoundCloudトークンを初期化しました user_id: #{current_user.id}"
     end
-    Rails.logger.info "current_user: #{current_user.inspect}"
     super
-    Rails.logger.info "AFTER super: flash: #{flash.inspect}"
   end
 
   def new
-    @emotion_logs = EmotionLog.includes(:user, :bookmarks, :tags).order(date: :desc).limit(10)
-    @mypage_title = "ようこそ音楽と感情を自由に表現する世界へ"
-    redirect_to emotion_logs_path, notice: flash[:notice]
+    redirect_to emotion_logs_path
   end
 
   protected
 
-  def after_sign_out_path_for(resource_or_scope)
+  def after_sign_out_path_for(_resource_or_scope)
     flash[:notice] = "ログアウトしました。また来てね！"
-    if mobile_device?
-      emotion_logs_path(view: "mobile")
-    else
-      emotion_logs_path
-    end
+    mobile_device? ? emotion_logs_path(view: "mobile") : emotion_logs_path
   end
 end
