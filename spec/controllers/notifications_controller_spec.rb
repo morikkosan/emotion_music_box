ab871d52fd5c9553365192a16f58d17d0ee99e68
moritti <morikko0124@yahoo.co.jp>
2025-09-18 21:36:13 +0900

    refactor: notificationsのリファクタリング　#98

--- spec/controllers/notifications_controller_spec.rb
+++ spec/controllers/notifications_controller_spec.rb
@@ -1,10 +1,17 @@
-require 'rails_helper'
-require 'web-push'
-
 RSpec.describe NotificationsController, type: :controller do
   let(:user) { create(:user) }
 
-  # Deviseのマッピング＋ログインを各ケースで使えるように
+  around do |example|
+    orig_pub  = ENV['VAPID_PUBLIC_KEY']
+    orig_priv = ENV['VAPID_PRIVATE_KEY']
+    begin
+      example.run
+    ensure
+      ENV['VAPID_PUBLIC_KEY']  = orig_pub
+      ENV['VAPID_PRIVATE_KEY'] = orig_priv
+    end
+  end
+
   before do
     @request.env['devise.mapping'] = Devise.mappings[:user]
   end
@@ -14,6 +21,9 @@ RSpec.describe NotificationsController, type: :controller do
       before do
         sign_in user
         create(:push_subscription, user: user)
+        # ここでキーを用意（send_webpush がショートサーキットしないように）
+        ENV['VAPID_PUBLIC_KEY']  = 'test_public_key'
+        ENV['VAPID_PRIVATE_KEY'] = 'test_private_key'
         allow(WebPush).to receive(:payload_send).and_return(true)
       end
 
@@ -28,25 +38,35 @@ RSpec.describe NotificationsController, type: :controller do
     context "プッシュ購読情報がない場合" do
       before { sign_in user }
 
-      it "通知を送信せず、メッセージを返す" do
+      it "通知を送信せず、422＋メッセージを返す" do
         get :test, params: { id: user.id }
-        expect(response).to have_http_status(:success)
+        expect(response).to have_http_status(:unprocessable_entity)
         expect(response.body).to eq("No subscription")
       end
     end
   end
 
   describe "GET #public_key" do
-    before do
-      ENV['VAPID_PUBLIC_KEY'] = 'test_public_key'
-      sign_in user
+    before { sign_in user }
+
+    context "VAPID公開鍵が設定されている場合" do
+      before { ENV['VAPID_PUBLIC_KEY'] = 'test_public_key' }
+
+      it "200で鍵を返す" do
+        get :public_key
+        expect(response).to have_http_status(:success)
+        expect(JSON.parse(response.body)["publicKey"]).to eq("test_public_key")
+      end
     end
 
-    it "VAPID公開鍵を返す" do
-      get :public_key
-      json = JSON.parse(response.body)
-      expect(response).to have_http_status(:success)
-      expect(json["publicKey"]).to eq("test_public_key")
+    context "VAPID公開鍵が未設定の場合" do
+      before { ENV['VAPID_PUBLIC_KEY'] = nil }
+
+      it "204 No Content を返す" do
+        get :public_key
+        expect(response).to have_http_status(:no_content)
+        expect(response.body).to be_blank
+      end
     end
   end
 end
