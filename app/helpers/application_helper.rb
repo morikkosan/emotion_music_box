91077af82a840ba589e5016f7fdcac5d6bf34012
moritti <morikko0124@yahoo.co.jp>
2025-10-15 22:00:49 +0900

    fix: サイトマップ修正 #90

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
+# app/helpers/application_helper.rb
 module ApplicationHelper
+  # 現在のURLから ?以降（クエリ）と #以降（フラグメント）を取り除いた“正規化URL”を返す
+  def canonical_url
+    url = request.original_url
+    uri = URI.parse(url)
+    uri.query = nil
+    uri.fragment = nil
+    uri.to_s
+  end
 end
